99655f2c2953e47745ce8b47e031b38a4d8506f3
Anton Davydov <fetsorn@gmail.com>
2026-05-06 12:57:55 +0300

    js: 2 spaces editorconfig, settle in delete

--- js/src/index.js
+++ js/src/index.js
@@ -1,95 +1,84 @@
-import { deleteRecord, createCatalog } from "@/proxy/record.js";
-import { buildRecord, updateRecord } from "@/proxy/impure.js";
-import { open } from "@/proxy/store.js";
-import db from "@/db.js";
+import { updateRecord } from "@/proxy/impure.js";
 import storageCSVS from "@/storage.js";
 import catalogIO from "@/catalog.js";
-import git from "@/git.js";
 import federationGit from "@/federation.js";
 
 async function SELECT({ fs, dir, catalog }, mind, query) {
-    const dirMind = await catalog.locate(mind);
+  const dirMind = await catalog.locate(mind);
 
-    const storage = storageCSVS(fs, dirMind);
+  const storage = storageCSVS(fs, dirMind);
 
-    return storage.sparql({ kind: "SELECT", query });
+  return storage.sparql({ kind: "SELECT", query });
 }
 
 async function DESCRIBE({ fs, dir, catalog }, mind, query) {
-    const dirMind = await catalog.locate(mind);
+  const dirMind = await catalog.locate(mind);
 
-    const storage = storageCSVS(fs, dirMind);
+  const storage = storageCSVS(fs, dirMind);
 
-    return storage.sparql({ kind: "DESCRIBE", query });
+  return storage.sparql({ kind: "DESCRIBE", query });
 }
 
 async function DELETE({ fs, dir, federation, catalog }, mind, query) {
-    const dirMind = await catalog.locate(mind);
+  const dirMind = await catalog.locate(mind);
 
-    const storage = storageCSVS(fs, dirMind);
+  const storage = storageCSVS(fs, dirMind);
 
-    await storage.sparql({ kind: "DELETE", query });
+  await storage.sparql({ kind: "DELETE", query });
 
-    //await federation.settle();
-    await git.commit(fs, mind);
+  await federation.settle(dirMind);
 
-    try {
-        await git.resolve(fs, mind);
-    } catch {
-        //do nothing
-    }
-
-    if (mind === "root") return catalog.retire(query);
+  if (mind === "root") return catalog.retire(query);
 }
 
 async function UPDATE({ fs, dir, storage, federation }, mind, query) {
-    //const dirMind = await catalog.locate(mind);
+  //const dirMind = await catalog.locate(mind);
 
-    //await storage.sparql(dirMind, { kind: "UPDATE", query });
-    await updateRecord(fs, mind, query);
+  //await storage.sparql(dirMind, { kind: "UPDATE", query });
+  await updateRecord(fs, mind, query);
 
-    //await federation.settle();
-    //await git.commit(fs, mind);
+  //await federation.settle();
+  //await git.commit(fs, mind);
 
-    //try {
-    //    await git.resolve(fs, mind);
-    //} catch {
-    //    //do nothing
-    //}
+  //try {
+  //    await git.resolve(fs, mind);
+  //} catch {
+  //    //do nothing
+  //}
 
-    //if (graph === "root") return catalog.induct(query);
+  //if (graph === "root") return catalog.induct(query);
 }
 
 async function sparql(providers, { kind, graph, query }) {
-    // TODO accept sparql string and infer kind with haydee
-    // const { kind, graph, inner } = await haydee.classify(sparql);
+  // TODO accept sparql string and infer kind with haydee
+  // const { kind, graph, inner } = await haydee.classify(sparql);
 
-    switch (kind) {
-        case "SELECT":
-            return SELECT(providers, graph, query);
+  switch (kind) {
+    case "SELECT":
+      return SELECT(providers, graph, query);
 
-        case "DESCRIBE":
-            return DESCRIBE(providers, graph, query);
+    case "DESCRIBE":
+      return DESCRIBE(providers, graph, query);
 
-        case "UPDATE":
-            return UPDATE(providers, graph, query);
+    case "UPDATE":
+      return UPDATE(providers, graph, query);
 
-        case "DELETE":
-            await DELETE(providers, graph, query);
-    }
+    case "DELETE":
+      await DELETE(providers, graph, query);
+  }
 }
 
 export default async function createMindZoo({ fs, dir }) {
-    const federation = federationGit(fs);
+  const federation = federationGit(fs);
 
-    const catalog = catalogIO({ fs, dir, federation });
+  const catalog = catalogIO({ fs, dir, federation });
 
-    await catalog.rebuild();
+  await catalog.rebuild();
 
-    const providers = { fs, dir, catalog, federation };
+  const providers = { fs, dir, catalog, federation };
 
-    return {
-        ...providers,
-        sparql: (query) => sparql(providers, query),
-    };
+  return {
+    ...providers,
+    sparql: (query) => sparql(providers, query),
+  };
 }
--- js/src/pure.js
+++ js/src/pure.js
@@ -1,56 +1,56 @@
 export function recordsToMind(
-    uuid,
-    name,
-    schemaRecord,
-    metaRecords,
-    url,
-    token,
+  uuid,
+  name,
+  schemaRecord,
+  metaRecords,
+  url,
+  token,
 ) {
-    // [[branch1, [branch2]]]
-    const schemaRelations = Object.entries(schemaRecord).filter(
-        ([key]) => key !== "_",
-    );
+  // [[branch1, [branch2]]]
+  const schemaRelations = Object.entries(schemaRecord).filter(
+    ([key]) => key !== "_",
+  );
 
-    // list of unique branches in the schema
-    const branches = [...new Set(schemaRelations.flat(Infinity))];
+  // list of unique branches in the schema
+  const branches = [...new Set(schemaRelations.flat(Infinity))];
 
-    // for each branch in the schema, put trunk into its metarecord
-    const branchRecords = branches.map((branch) => {
-        const metaRecord =
-            metaRecords.find((record) => record.branch === branch) ?? {};
+  // for each branch in the schema, put trunk into its metarecord
+  const branchRecords = branches.map((branch) => {
+    const metaRecord =
+      metaRecords.find((record) => record.branch === branch) ?? {};
 
-        const trunks = schemaRelations.reduce((accTrunk, [trunk, leaves]) => {
-            const trunkPartial = leaves.includes(branch) ? [trunk] : [];
+    const trunks = schemaRelations.reduce((accTrunk, [trunk, leaves]) => {
+      const trunkPartial = leaves.includes(branch) ? [trunk] : [];
 
-            return [...accTrunk, ...trunkPartial];
-        }, []);
+      return [...accTrunk, ...trunkPartial];
+    }, []);
 
-        const trunkPartial = trunks.length > 0 ? { trunk: trunks } : {};
+    const trunkPartial = trunks.length > 0 ? { trunk: trunks } : {};
 
-        return { ...metaRecord, ...trunkPartial };
-    });
+    return { ...metaRecord, ...trunkPartial };
+  });
 
-    const tokenPartial = token !== undefined ? { origin_token: token } : {};
+  const tokenPartial = token !== undefined ? { origin_token: token } : {};
 
-    const originPartial =
-        url !== undefined
-            ? {
-                  origin_url: {
-                      _: "origin_url",
-                      origin_url: url,
-                      ...tokenPartial,
-                  },
-              }
-            : {};
+  const originPartial =
+    url !== undefined
+      ? {
+          origin_url: {
+            _: "origin_url",
+            origin_url: url,
+            ...tokenPartial,
+          },
+        }
+      : {};
 
-    const mind = {
-        _: "mind",
-        mind: uuid,
-        name,
-        branch: branchRecords,
-    };
+  const mind = {
+    _: "mind",
+    mind: uuid,
+    name,
+    branch: branchRecords,
+  };
 
-    return mind;
+  return mind;
 }
 
 /**
@@ -61,50 +61,50 @@ export function recordsToMind(
  * @returns {object}
  */
 export function schemaToBranchRecords(schema) {
-    const records = Object.entries(schema).reduce(
-        (withEntry, [branch, { leaves, task, cognate, description }]) => {
-            const leavesPartial = withEntry.schemaRecord[branch] ?? [];
+  const records = Object.entries(schema).reduce(
+    (withEntry, [branch, { leaves, task, cognate, description }]) => {
+      const leavesPartial = withEntry.schemaRecord[branch] ?? [];
 
-            const schemaRecord =
-                leaves.length > 0
-                    ? {
-                          ...withEntry.schemaRecord,
-                          [branch]: [...new Set([...leaves, ...leavesPartial])],
-                      }
-                    : withEntry.schemaRecord;
+      const schemaRecord =
+        leaves.length > 0
+          ? {
+              ...withEntry.schemaRecord,
+              [branch]: [...new Set([...leaves, ...leavesPartial])],
+            }
+          : withEntry.schemaRecord;
 
-            const partialEn =
-                description && description.en !== undefined
-                    ? { description_en: description.en }
-                    : {};
+      const partialEn =
+        description && description.en !== undefined
+          ? { description_en: description.en }
+          : {};
 
-            const partialRu =
-                description && description.ru !== undefined
-                    ? { description_ru: description.ru }
-                    : {};
+      const partialRu =
+        description && description.ru !== undefined
+          ? { description_ru: description.ru }
+          : {};
 
-            const partialTask = task ? { task } : {};
+      const partialTask = task ? { task } : {};
 
-            const partialCognate = cognate ? { cognate } : {};
+      const partialCognate = cognate ? { cognate } : {};
 
-            const metaRecords = [
-                ...withEntry.metaRecords,
-                {
-                    _: "branch",
-                    branch,
-                    ...partialTask,
-                    ...partialCognate,
-                    ...partialEn,
-                    ...partialRu,
-                },
-            ];
-
-            return { schemaRecord, metaRecords };
+      const metaRecords = [
+        ...withEntry.metaRecords,
+        {
+          _: "branch",
+          branch,
+          ...partialTask,
+          ...partialCognate,
+          ...partialEn,
+          ...partialRu,
         },
-        { schemaRecord: { _: "_" }, metaRecords: [] },
-    );
+      ];
 
-    return [records.schemaRecord, ...records.metaRecords];
+      return { schemaRecord, metaRecords };
+    },
+    { schemaRecord: { _: "_" }, metaRecords: [] },
+  );
+
+  return [records.schemaRecord, ...records.metaRecords];
 }
 
 /**
@@ -115,34 +115,31 @@ export function schemaToBranchRecords(schema) {
  * @returns {object[]}
  */
 export function extractSchemaRecords(branchRecords) {
-    const records = branchRecords.reduce(
-        (withBranch, branchRecord) => {
-            const { trunk, leaf: omit, ...branchRecordOmitted } = branchRecord;
+  const records = branchRecords.reduce(
+    (withBranch, branchRecord) => {
+      const { trunk, leaf: omit, ...branchRecordOmitted } = branchRecord;
 
-            const trunks = Array.isArray(trunk) ? trunk : [trunk];
+      const trunks = Array.isArray(trunk) ? trunk : [trunk];
 
-            const schemaRecord = trunks
-                .filter((t) => t !== undefined)
-                .reduce((withTrunk, trunk) => {
-                    const leaves = withBranch.schemaRecord[trunk] ?? [];
+      const schemaRecord = trunks
+        .filter((t) => t !== undefined)
+        .reduce((withTrunk, trunk) => {
+          const leaves = withBranch.schemaRecord[trunk] ?? [];
 
-                    const schemaRecord = {
-                        ...withBranch.schemaRecord,
-                        [trunk]: [...new Set([branchRecord.branch, ...leaves])],
-                    };
+          const schemaRecord = {
+            ...withBranch.schemaRecord,
+            [trunk]: [...new Set([branchRecord.branch, ...leaves])],
+          };
 
-                    return schemaRecord;
-                }, withBranch.schemaRecord);
+          return schemaRecord;
+        }, withBranch.schemaRecord);
 
-            const metaRecords = [
-                branchRecordOmitted,
-                ...withBranch.metaRecords,
-            ];
+      const metaRecords = [branchRecordOmitted, ...withBranch.metaRecords];
 
-            return { schemaRecord, metaRecords };
-        },
-        { schemaRecord: { _: "_" }, metaRecords: [] },
-    );
+      return { schemaRecord, metaRecords };
+    },
+    { schemaRecord: { _: "_" }, metaRecords: [] },
+  );
 
-    return [records.schemaRecord, ...records.metaRecords];
+  return [records.schemaRecord, ...records.metaRecords];
 }
